6b42c7ff32a7230095eaf93b6b00bf480e5052d1
TheJonaCode <mcjhona069@gmail.com>
2022-11-21 21:34:41 -0600

    share API

--- twittor-recursos-nativos/public/js/app.js
+++ twittor-recursos-nativos/public/js/app.js
@@ -87,6 +87,8 @@ function crearMensajeHTML(mensaje, personaje, lat, lng, foto) {
 
     var content = `
     <li class="animated fadeIn fast"
+    data-user="${ personaje }"
+    data-mensaje="${ mensaje }"
         data-tipo="mensaje">
 
 
@@ -527,3 +529,37 @@ btnTomarFoto.on('click', () => {
 
 
 // Share API
+
+// if ( navigator.share ) {
+//     console.log('Navegador lo soporta');
+// } else {
+//     console.log('Navegador NO lo soporta');
+// }
+
+timeline.on('click', 'li', function() {
+    // console.log( $(this) );
+    // console.log( $(this).data('tipo') );
+    // console.log( $(this).data('user') );
+
+    let tipo = $(this).data('tipo');
+    let lat = $(this).data('lat');
+    let lng = $(this).data('lng');
+    let mensaje = $(this).data('mensaje');
+    let user = $(this).data('user');
+
+    console.log({ tipo, lat, lng, mensaje, user });
+
+    const shareOpts = {
+        title: user,
+        text: mensaje
+    };
+
+    if (tipo === 'mapa') {
+        shareOpts.text = 'Mapa';
+        shareOpts.url = `https://www.google.com/maps/@${ lat },${ lng },15z`;
+    }
+
+    navigator.share(shareOpts)
+        .then(() => console.log('Successful share'))
+        .catch((error) => console.log('Error sharing', error));
+});
